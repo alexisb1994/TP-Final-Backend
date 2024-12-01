645b6a60a83502b4cd01ec4d251623e53ce59920
alexisb1994 <alexis__blanco@hotmail.com>
2024-12-01 18:13:20 -0300

    add:filmRoutes

--- src/routes/filmRoutes.js
+++ src/routes/filmRoutes.js
@@ -3,5 +3,11 @@ import{ Router } from "express";
 
 const FilmRoutes=Router();
 
+//api/films
+FilmRoutes.get("/",getAllFilms)
+FilmRoutes.get("/:id",getFilmById)
+FilmRoutes.post("/",createFilm)
+FilmRoutes.patch("/:id",updateFilm)
+FilmRoutes.delete(":/id",deleteFilm)
 
 export {FilmRoutes}
